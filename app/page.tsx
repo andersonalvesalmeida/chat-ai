00df5c8ac0eb46b2dbff77a909ec936dae4bf323
Anderson Alves de Almeida <poolcity@gmail.com>
2025-04-21 21:57:18 -0300

    feat: Add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-50">
       <ChatAI />
+      <div>Funcionalidades do modo Dark</div>
     </main>
   );
 }
